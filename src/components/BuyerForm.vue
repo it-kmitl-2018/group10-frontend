2bac50b632897d78301886a1e64a6675ab845709
Pattiiz <phat.my.name@gmail.com>
2018-04-20 00:59:11 +0700

    Edit white space

--- src/components/BuyerForm.vue
+++ src/components/BuyerForm.vue
@@ -3,23 +3,23 @@
   <h2>ผู้ซื้อ</h2>
     <label>รหัสผู้ค้า (ผู้ซื้อ) : </label><input type="text"/><br>
     <label>รหัสผู้ค้าสากล (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>ชื่อผู้ซื้อ : </label><input type="text"/><br>
-            <label>เลขประจำตัวผู้เสียภาษีอากร (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>รหัสสาขา (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>อีเมล (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>ชื่ออาคาร (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>บ้านเลขที่ (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>ซอย (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>หมู่บ้าน (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>หมู่ที่ (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>ถนน (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>ตำบล (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>อำเภอ (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>จังหวัด (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>รหัสประเทศ (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>รหัสไปรษณีย์ (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>ที่อยู่บรรทัดที่ 1 (ผู้ซื้อ) : </label><input type="text"/><br>
-            <label>ที่อยู่บรรทัดที่ 2 (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>ชื่อผู้ซื้อ : </label><input type="text"/><br>
+    <label>เลขประจำตัวผู้เสียภาษีอากร (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>รหัสสาขา (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>อีเมล (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>ชื่ออาคาร (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>บ้านเลขที่ (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>ซอย (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>หมู่บ้าน (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>หมู่ที่ (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>ถนน (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>ตำบล (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>อำเภอ (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>จังหวัด (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>รหัสประเทศ (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>รหัสไปรษณีย์ (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>ที่อยู่บรรทัดที่ 1 (ผู้ซื้อ) : </label><input type="text"/><br>
+    <label>ที่อยู่บรรทัดที่ 2 (ผู้ซื้อ) : </label><input type="text"/><br>
 </div>
 </template>
 
