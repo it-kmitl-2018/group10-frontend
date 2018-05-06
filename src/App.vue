fb11a72b69b4f6e3d11081e7ba409365c6bda293
aliveable <aliveable1754@gmail.com>
2018-05-06 19:23:44 +0700

    add EReceiptForm form page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div>
+        <router-link :to="{name: 'app'}"> หน้าแรก </router-link>
+        <router-link :to="{name: 'ElectronicReceiptForm'}"> ใบเสร็จ </router-link>
+        <!-- <router-link :to="{name: 'EReceiptForm'}"> ใบเสร็จ </router-link>
+        <router-link :to="{name: 'ETaxInvoiceForm'}"> ใบแจ้งหนี้ </router-link> -->
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
