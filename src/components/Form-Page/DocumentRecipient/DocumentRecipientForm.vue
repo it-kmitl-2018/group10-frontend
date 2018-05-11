d5ba43071c84c9e8702aa16191e270b433ea9e2d
Unknown <kuro-neko@hotmail.co.th>
2018-05-11 13:05:54 +0700

    Group data

--- src/components/Form-Page/DocumentRecipient/DocumentRecipientForm.vue
+++ src/components/Form-Page/DocumentRecipient/DocumentRecipientForm.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
     <h2>ผู้รับเอกสาร</h2>
-    <invoice-trade-party :documentRecipientFormProp="documentRecipient"></invoice-trade-party>
-    <specified-tax-registration :documentRecipientFormProp="documentRecipient"></specified-tax-registration>
-    <defined-trade-contact :documentRecipientFormProp="documentRecipient"></defined-trade-contact>
-    <email-and-phone :documentRecipientFormProp="documentRecipient"></email-and-phone>
-    <postal-trade-address :documentRecipientFormProp="documentRecipient"></postal-trade-address>
+    <invoicee-trade-party :invoiceeTradePartyProp="documentRecipient.invoiceeTradeParty"></invoicee-trade-party>
+    <specified-tax-registration :specifiedTaxRegistrationProp="documentRecipient.specifiedTaxRegistration"></specified-tax-registration>
+    <email-and-phone :emailAndPhoneProp="documentRecipient.emailAndPhone"></email-and-phone>
+    <postal-trade-address :postalTradeAddressProp="documentRecipient.postalTradeAddress"></postal-trade-address>
   </div>
 </template>
 
 <script>
-import InvoiceTradeParty from '@/components/Form-Page/DocumentRecipient/components/InvoiceTradeParty.vue'
+import InvoiceeTradeParty from '@/components/Form-Page/DocumentRecipient/components/InvoiceeTradeParty.vue'
 import SpecifiedTaxRegistration from '@/components/Form-Page/DocumentRecipient/components/SpecifiedTaxRegistration.vue'
-import DefinedTradeContact from '@/components/Form-Page/DocumentRecipient/components/DefinedTradeContact.vue'
 import EmailAndPhone from '@/components/Form-Page/DocumentRecipient/components/EmailAndPhone.vue'
 import PostalTradeAddress from '@/components/Form-Page/DocumentRecipient/components/PostalTradeAddress.vue'
 import DocumentRecipientData from '@/Data/DocumentRecipient.data.js'
@@ -23,9 +21,8 @@ export default {
     }
   },
   components: {
-    InvoiceTradeParty,
+    InvoiceeTradeParty,
     SpecifiedTaxRegistration,
-    DefinedTradeContact,
     EmailAndPhone,
     PostalTradeAddress
   },
