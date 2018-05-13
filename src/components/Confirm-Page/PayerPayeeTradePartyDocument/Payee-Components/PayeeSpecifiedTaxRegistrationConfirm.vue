1be86e86bc866bfd52152b5031bd2c74a8ea44ef
aliveable <aliveable1754@gmail.com>
2018-05-13 23:03:05 +0700

    Add PayeeconfirmPageComponent with their confirm form

---
+++ src/components/Confirm-Page/PayerPayeeTradePartyDocument/Payee-Components/PayeeSpecifiedTaxRegistrationConfirm.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+      <label>เลขประจำตัวผู้เสียภาษีอากร (ผู้รับชำระเงิน) : </label><input type="text" v-model="ConfirmPayeeSpecifiedTaxRegistrationProp.TaxID"/><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['ConfirmPayeeSpecifiedTaxRegistrationProp']
+}
+</script>
---
+++ src/components/Confirm-Page/PayerPayeeTradePartyDocument/Payee-Components/PayeeTradePartyConfirmForm.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="payeetradepartyform">
+  <h2>ผู้รับชำระเงิน</h2>
+    <PayeeTradePartyConfirm :ConfirmPayeeTradePartyProp="ConfirmPayeeProps.payerPayeeTradeParty"></PayeeTradePartyConfirm>
+    <PayeeEmailConfirm :ConfirmPayeeEmailProp="ConfirmPayeeProps.payerPayeeEmail"></PayeeEmailConfirm>
+    <PayeeSpecifiedTaxRegistrationConfirm :ConfirmPayeeSpecifiedTaxRegistrationProp="ConfirmPayeeProps.payerPayeeSpecifiedTaxRegistration"></PayeeSpecifiedTaxRegistrationConfirm>
+    <PayeeTradePartyAddressConfirm :ConfirmPayeeTradePartyAddressProp="ConfirmPayeeProps.payerPayeeTradePartyAddress"></PayeeTradePartyAddressConfirm>
+
+</div>
+</template>
+
+<script>
+import PayeeEmailConfirm from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeEmail.vue'
+import PayeeSpecifiedTaxRegistrationConfirm from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeSpecifiedTaxRegistration.vue'
+import PayeeTradePartyConfirm from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradeParty.vue'
+import PayeeTradePartyAddressConfirm from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradePartyAddress.vue'
+export default{
+  components: {
+    PayeeEmailConfirm,
+    PayeeSpecifiedTaxRegistrationConfirm,
+    PayeeTradePartyConfirm,
+    PayeeTradePartyAddressConfirm
+  },
+  props: ['ConfirmPayeeProps']
+}
+</script>
