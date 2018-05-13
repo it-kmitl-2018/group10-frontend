a95fe25a8102aa8e547dde5a4f7cd46951bc8f9e
aliveable <aliveable1754@gmail.com>
2018-05-13 23:54:30 +0700

    add Payeeconfirmpagecomponent and assembling page on PayeeTradePartyConfirmForm

---
+++ src/components/Comfirm-Page/PayerPayeeTradePartyDocument/Payee-Components/PayeeTradePartyConfirm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <label>รหัสผู้รับชำระเงิน : </label><input type="text" v-model="ConfirmPayeeTradePartyProp.id" /><br>
+    <label>รหัสสากล (ผู้รับชำระเงิน) : </label><input type="text" v-model="ConfirmPayeeTradePartyProp.GlobalID"/><br>
+    <label>ชื่อผู้รับชำระเงิน : </label><input type="text" v-model="ConfirmPayeeTradePartyProp.Name"/><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['ConfirmPayeeTradePartyProp']
+}
+</script>
---
+++ src/components/Comfirm-Page/PayerPayeeTradePartyDocument/Payee-Components/PayeeTradePartyConfirmForm.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="payeetradepartyform">
+  <h2>ผู้รับชำระเงิน</h2>
+    <PayeeTradePartyConfirm :ConfirmPayeeTradePartyProp="ConfirmPayeeProps.payerPayeeTradeParty"></PayeeTradePartyConfirm>
+    <PayeeEmailConfirm :ConfirmPayeeEmailProp="ConfirmPayeeProps.payerPayeeEmail"></PayeeEmailConfirm>
+    <PayeeSpecifiedTaxRegistrationConfirm :ConfirmPayeeSpecifiedTaxRegistrationProp="ConfirmPayeeProps.payerPayeeSpecifiedTaxRegistration"></PayeeSpecifiedTaxRegistrationConfirm>
+    <PayeeTradePartyAddressConfirm :ConfirmPayeeTradePartyAddressProp="ConfirmPayeeProps.payerPayeeTradePartyAddress"></PayeeTradePartyAddressConfirm>
+
+</div>
+</template>
+
+<script>
+import PayeeEmailConfirm from '@/components/Confirm-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeEmailConfirm.vue'
+import PayeeSpecifiedTaxRegistrationConfirm from '@/components/Confirm-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeSpecifiedTaxRegistrationConfirm.vue'
+import PayeeTradePartyConfirm from '@/components/Confirm-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradePartyConfirm.vue'
+import PayeeTradePartyAddressConfirm from '@/components/Confirm-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradePartyAddressConfirm.vue'
+export default{
+  components: {
+    PayeeEmailConfirm,
+    PayeeSpecifiedTaxRegistrationConfirm,
+    PayeeTradePartyConfirm,
+    PayeeTradePartyAddressConfirm
+  },
+  props: ['ConfirmPayeeProps']
+}
+</script>
