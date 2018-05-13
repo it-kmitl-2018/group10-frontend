4c3849da92b6966cc76784538fc0638c5976ae05
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 22:56:42 +0700

    Create RecipientConfirm
    สร้าง components Email, InvoiceTradeParty, SpecifiedTaxRegistration, PostalTradeAddress และประกอบเป็น RecipientConfirm

---
+++ src/components/Confirm-Page/Recipient/RecipientConfirm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>ผู้รับ</h2>
+    <invoice-trade-party :invoiceTradePartyConfirm="recipientConfirmProp.invoiceTradeParty"></invoice-trade-party>
+    <specified-tax-registration :specifiedTaxRegistrationConfirm="recipientConfirmProp.specifiedTaxRegistration"></specified-tax-registration>
+    <email :emailConfirm="recipientConfirmProp.email"></email>
+    <postal-trade-address :postalTradeAddressConfirm="recipientConfirmProp.postalTradeAddress"></postal-trade-address>
+  </div>
+</template>
+
+<script>
+import InvoiceTradeParty from '@/components/Confirm-Page/Recipient/components/InvoiceTradeParty.vue'
+import SpecifiedTaxRegistration from '@/components/Confirm-Page/Recipient/components/SpecifiedTaxRegistration.vue'
+import Email from '@/components/Confirm-Page/Recipient/components/Email.vue'
+import PostalTradeAddress from '@/components/Confirm-Page/Recipient/components/PostalTradeAddress.vue'
+
+export default {
+  components: {
+    InvoiceTradeParty,
+    SpecifiedTaxRegistration,
+    Email,
+    PostalTradeAddress
+  },
+  props: ['recipientConfirmProp']
+}
+</script>
---
+++ src/components/Confirm-Page/Recipient/components/PostalTradeAddress.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    รหัสไปรษณีย์: <span>{{postalTradeAddressConfirm.postalCode }}</span><br>
+    ชื่ออาคาร: <span>{{postalTradeAddressConfirm.building }}</span><br>
+    ที่อยู่บรรทัดที่ 1: <span>{{postalTradeAddressConfirm.addressLineOne}}</span><br>
+    ที่อยู่บรรทัดที่ 2: <span>{{postalTradeAddressConfirm.addressLineTwo}}</span><br>
+    ซอย: <span>{{postalTradeAddressConfirm.addressLineThree}}</span><br>
+    หมู่บ้าน: <span>{{postalTradeAddressConfirm.addressLineFour}}</span><br>
+    หมู่ที่: <span>{{postalTradeAddressConfirm.addressLineFive}}</span><br>
+    ถนน: <span>{{postalTradeAddressConfirm.street}}</span><br>
+    ชื่ออำเภอ: <span>{{postalTradeAddressConfirm.district}}</span><br>
+    ชื่อตำบล: <span>{{postalTradeAddressConfirm.subDistrict}}</span><br>
+    รหัสประเทศ: <span>{{postalTradeAddressConfirm.countryID}}</span><br>
+    รหัสจังหวัด: <span>{{postalTradeAddressConfirm.provinceID}}</span><br>
+    บ้านเลขที่: <span>{{postalTradeAddressConfirm.houseNumber}}</span><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['postalTradeAddressConfirm']
+}
+</script>
