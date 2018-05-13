14eedb5b4b97da91a0e3aea457b33d6fbe1ca2fd
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 23:22:20 +0700

    Create TaxInvoiceConfirm

---
+++ src/components/Confirm-Page/TaxInvoice/TaxInvoiceConfirm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>ข้อมูลภาษี</h2>
+    รหัสประเภทภาษี : <span>{{taxInvoiceConfirm.taxCode}}</span><br>
+    อัตราภาษี : <span>{{taxInvoiceConfirm.taxInvoiceRate}}</span><br>
+    มูลค่าสินค้าบริการ (ไม่รวมภาษี) : <span>{{taxInvoiceConfirm.valueAmount}}</span><br>
+    มูลต่าภาษีมูลค่าเพิ่ม: <span>{{taxInvoiceConfirm.vatAmount}}</span><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['taxInvoiceConfirm']
+}
+</script>
