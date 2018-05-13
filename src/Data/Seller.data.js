050e119c69972d8243ebd8c7991aa380d3debe46
Pattiiz <phat.my.name@gmail.com>
2018-05-13 14:54:04 +0700

    Create SellerForm component with data object

---
+++ src/Data/Seller.data.js
@@ -0,0 +1,28 @@
+export default {
+  data () {
+    return {
+      seller: {
+        id: '',
+        globalID: '',
+        name: '',
+        taxID: '',
+        branchNo: '',
+        email: '',
+        buildingName: '',
+        buildingNo: '',
+        lineThree: '',
+        alley: '',
+        village: '',
+        villageNo: '',
+        street: '',
+        subDistrict: '',
+        district: '',
+        subCountry: '',
+        country: '',
+        postcode: '',
+        addrLineOne: '',
+        addrLineTwo: ''
+      }
+    }
+  }
+}
---
+++ src/components/Form-Page/SellerForm/SellerForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="SellerForm">
+    <h2>ผู้ขาย</h2>
+    <label>รหัสผู้ค้า (ผู้ขาย) : </label><input type="text" v-model="sellerform.id"/><br>
+    <label>รหัสผู้ค้าสากล (ผู้ขาย) : </label><input type="text" v-model="sellerform.globalID"/><br>
+    <label>ชื่อผู้ขาย : </label><input type="text" v-model="sellerform.name"/><br>
+    <label>เลขประจำตัวผู้เสียภาษีอากร (ผู้ขาย) : </label><input type="text" v-model="sellerform.taxID"/><br>
+    <label>รหัสสาขา (ผู้ขาย) : </label><input type="text" v-model="sellerform.branchNo"/><br>
+    <label>อีเมล (ผู้ขาย) : </label><input type="text" v-model="sellerform.email"/><br>
+    <label>ชื่ออาคาร (ผู้ขาย) : </label><input type="text" v-model="sellerform.buildingName"/><br>
+    <label>บ้านเลขที่ (ผู้ขาย) : </label><input type="text" v-model="sellerform.buildingNo"/><br>
+    <label>ซอย (ผู้ขาย) : </label><input type="text" v-model="sellerform.alley"/><br>
+    <label>หมู่บ้าน (ผู้ขาย) : </label><input type="text" v-model="sellerform.village"/><br>
+    <label>หมู่ที่ (ผู้ขาย) : </label><input type="text" v-model="sellerform.villageNo"/><br>
+    <label>ถนน (ผู้ขาย) : </label><input type="text" v-model="sellerform.street"/><br>
+    <label>ตำบล (ผู้ขาย) : </label><input type="text" v-model="sellerform.subDistrict"/><br>
+    <label>อำเภอ (ผู้ขาย) : </label><input type="text" v-model="sellerform.district"/><br>
+    <label>จังหวัด (ผู้ขาย) : </label><input type="text" v-model="sellerform.subCountry"/><br>
+    <label>ประเทศ (ผู้ขาย) : </label><input type="text" v-model="sellerform.country"/><br>
+    <label>รหัสไปรษณีย์ (ผู้ขาย) : </label><input type="text" v-model="sellerform.postcode"/><br>
+    <label>ที่อยู่บรรทัดที่ 1 (ผู้ขาย) : </label><input type="text" v-model="sellerform.addrLineOne"/><br>
+    <label>ที่อยู่บรรทัดที่ 2 (ผู้ขาย) : </label><input type="text" v-model="sellerform.addrLineTwo"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sellerform',
+  props: ['sellerform']
+}
+</script>
+
+<style>
+
+</style>
