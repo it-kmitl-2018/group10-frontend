2ab241d1c38f6afcadc3425054e8041f77fa9caf
aliveable <aliveable1754@gmail.com>
2018-05-13 22:28:09 +0700

    Create Payer Comfirm Component

---
+++ src/components/Confirm-Page/PayerPayeeTradePartyDocument/Payer-components/PayerTradePartyConfirm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <label>รหัสผู้ชำระเงิน : </label><input type="text" v-model="ConfirmPayerTradePartyProp.id"/><br>
+    <label>รหัสสากล (ผู้ชำระเงิน) : </label><input type="text" v-model="ConfirmPayerTradePartyProp.GlobalID"/><br>
+    <label>ชื่อผู้ชำระเงิน : </label><input type="text" v-model="ConfirmPayerTradePartyProp.Name"/><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['ConfirmPayerTradePartyProp']
+}
+</script>
