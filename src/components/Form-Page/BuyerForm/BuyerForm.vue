ae6a1022757f707dfe0f68cae46dfa07d80b4cc2
Pattiiz <phat.my.name@gmail.com>
2018-05-14 02:32:00 +0700

    Create buyer component and data object

---
+++ src/components/Form-Page/BuyerForm/BuyerForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="buyerform">
+    <h2>ผู้ซื้อ</h2>
+    <information :information='buyerForm.information'></information>
+    <postal-address :postalAddress='buyerForm.postalAddress'></postal-address>
+  </div>
+</template>
+
+<script>
+import Information from '@/components/Form-Page/BuyerForm/components/Information.vue'
+import PostalAddress from '@/components/Form-Page/BuyerForm/components/PostalAddress.vue'
+
+export default {
+  props: ['buyerForm'],
+  components: {
+    Information,
+    PostalAddress
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Form-Page/BuyerForm/components/Information.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <label>รหัสผู้ค้า (ผู้ซื้อ) : </label><input type="text" v-model="information.id"/><br>
+    <label>รหัสผู้ค้าสากล (ผู้ซื้อ) : </label><input type="text" v-model="information.globalID"/><br>
+    <label>ชื่อผู้ซื้อ : </label><input type="text" v-model="information.name"/><br>
+    <label>เลขประจำตัวผู้เสียภาษีอากร (ผู้ซื้อ) : </label><input type="text" v-model="information.taxID"/><br>
+    <label>อีเมล (ผู้ซื้อ) : </label><input type="text" v-model="information.email"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['information']
+}
+</script>
+
+<style>
+
+</style>
