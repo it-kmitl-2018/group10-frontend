1655e94498a4b59b3b6f2bf9d102d1ee2bb5ee56
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 22:30:04 +0700

    Create DocumentRecipientConfirm

---
+++ src/components/Confirm-Page/DocumentRecipient/components/SpecifiedTaxRegistration.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    เลขประจำตัวผู้เสียภาษีอากร : <span>{{specifiedTaxRegistrationConfirm.taxID}}</span><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['specifiedTaxRegistrationConfirm']
+}
+</script>
