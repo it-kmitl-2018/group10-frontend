6de925751c573937b8cb9efea22eca249660b16a
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 22:43:54 +0700

    Edit InvoiceTradeParty.vue(DocumentRecipientConfirm)
    ลบคำว่า "เทส" ที่พิมพ์ทิ้งไว้ในไฟล์ InvoiceTradeParty.vue ในส่วนของDocumentRecipientConfirm ออก

--- src/components/Confirm-Page/DocumentRecipient/components/InvoiceTradeParty.vue
+++ src/components/Confirm-Page/DocumentRecipient/components/InvoiceTradeParty.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    เทส
     รหัสผู้ค้า(ผู้รับเอกสาร) : <span>{{invoiceTradePartyConfirm.id}}</span><br>
     รหัสผู้ค้าสากล(ผู้รับเอกสาร) : <span>{{invoiceTradePartyConfirm.globalID}}</span><br>
     ชื่อผู้ขาย : <span>{{invoiceTradePartyConfirm.name}}</span><br>
