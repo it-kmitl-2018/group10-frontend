7169e77d9fc33cd16c9371fb665ef1d98a8db9dd
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 16:03:30 +0700

    Create RecipientForm and dataobject
    Create RecipientForm.vue, Recipient.data.jss

---
+++ src/components/Form-Page/Recipient/RecipientForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>ผู้รับ</h2>
+    <ship-to-trade-party :shipToTradePartyProp="recipientFormProp.invoiceTradeParty"></ship-to-trade-party>
+    <specified-tax-registration :specifiedTaxRegistrationProp="recipientFormProp.specifiedTaxRegistration"></specified-tax-registration>
+    <email :emailProp="recipientFormProp.email"></email>
+    <postal-trade-address :postalTradeAddressProp="recipientFormProp.postalTradeAddress"></postal-trade-address>
+  </div>
+</template>
+
+<script>
+import ShipToTradeParty from '@/components/Form-Page/Recipient/components/ShipToTradeParty.vue'
+import SpecifiedTaxRegistration from '@/components/Form-Page/DocumentRecipient/components/SpecifiedTaxRegistration.vue'
+import Email from '@/components/Form-Page/Recipient/components/Email.vue'
+import PostalTradeAddress from '@/components/Form-Page/Recipient/components/PostalTradeAddress.vue'
+
+export default {
+  components: {
+    ShipToTradeParty,
+    SpecifiedTaxRegistration,
+    Email,
+    PostalTradeAddress
+  },
+  props: ['recipientFormProp']
+}
+</script>
