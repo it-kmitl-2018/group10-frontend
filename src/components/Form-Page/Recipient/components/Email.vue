76a7f41814ba53817e6b58e2a1b7847c122f80a7
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 15:58:24 +0700

    Create components for Recipient
    Create SpecifiedTaxRegistration.vue, ShipToTradeParty.vue, PostalTradeAddress.vue, Email.vue

---
+++ src/components/Form-Page/Recipient/components/Email.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    อีเมล: <input v-model="emailProp.email"/><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['emailProp']
+}
+</script>
---
+++ src/components/Form-Page/Recipient/components/ShipToTradeParty.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    รหัสผู้รับ : <input v-model="shipToTradePartyProp.id"/><br>
+    รหัสสากล : <input v-model="shipToTradePartyProp.globalID"/><br>
+    ชื่อผู้รับ : <input v-model="shipToTradePartyProp.name"/><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['shipToTradePartyProp']
+}
+</script>
