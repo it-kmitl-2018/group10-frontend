1d82561c7a6167a7655c9d132fafc3c4459474bb
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 22:59:09 +0700

    Edit InvoiceTradeParty(DocumentRecipientConfirm)
    เปลี่ยน "ชื่อผู้ขาย" เป็น "ชื่อผู้รับเอกสาร"

--- src/components/Confirm-Page/DocumentRecipient/components/InvoiceTradeParty.vue
+++ src/components/Confirm-Page/DocumentRecipient/components/InvoiceTradeParty.vue
@@ -2,7 +2,7 @@
   <div>
     รหัสผู้ค้า(ผู้รับเอกสาร) : <span>{{invoiceTradePartyConfirm.id}}</span><br>
     รหัสผู้ค้าสากล(ผู้รับเอกสาร) : <span>{{invoiceTradePartyConfirm.globalID}}</span><br>
-    ชื่อผู้ขาย : <span>{{invoiceTradePartyConfirm.name}}</span><br>
+    ชื่อผู้รับเอกสาร : <span>{{invoiceTradePartyConfirm.name}}</span><br>
   </div>
 </template>
 <script>
