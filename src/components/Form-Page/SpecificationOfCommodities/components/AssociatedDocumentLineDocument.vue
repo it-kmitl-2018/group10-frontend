7a042265bd758793a33c0c880bb4a395a55a04c2
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 20:15:52 +0700

    Create AssociatedDocumentLineDocument component

---
+++ src/components/Form-Page/SpecificationOfCommodities/components/AssociatedDocumentLineDocument.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    ลำดับรายการ : <input v-model="associatedDocumentLineDocumentProp.lineID"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['associatedDocumentLineDocumentProp']
+}
+</script>
