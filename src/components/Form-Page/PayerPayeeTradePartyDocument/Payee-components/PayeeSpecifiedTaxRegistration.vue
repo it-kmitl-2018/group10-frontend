c66f90878c1d286074a526f64de2d8a1d6246fef
aliveable <aliveable1754@gmail.com>
2018-05-14 18:42:37 +0700

    Create PayeeFormPage

---
+++ src/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeSpecifiedTaxRegistration.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+      <label>เลขประจำตัวผู้เสียภาษีอากร (ผู้รับชำระเงิน) : </label><input type="text" v-model="PayeeSpecifiedTaxRegistrationProp.TaxID"/><br>
+  </div>
+</template>
+<script>
+export default {
+  props: ['PayeeSpecifiedTaxRegistrationProp']
+}
+</script>
---
+++ src/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradePartyForm.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="payeetradepartyform">
+  <h2>ผู้รับชำระเงิน</h2>
+    <PayeeTradeParty :PayeeTradePartyProp="PayeeProps.payerPayeeTradeParty"></PayeeTradeParty>
+    <PayeeEmail :PayerEmailProp="PayeeProps.payerPayeeEmail"></PayeeEmail>
+    <PayeeSpecifiedTaxRegistration :PayeeSpecifiedTaxRegistrationProp="PayeeProps.payerPayeeSpecifiedTaxRegistration"></PayeeSpecifiedTaxRegistration>
+    <PayeeTradePartyAddress :PayeeTradePartyAddressProp="PayeeProps.payerPayeeTradePartyAddress"></PayeeTradePartyAddress>
+
+</div>
+</template>
+
+<script>
+import PayeeEmail from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeEmail.vue'
+import PayeeSpecifiedTaxRegistration from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeSpecifiedTaxRegistration.vue'
+import PayeeTradeParty from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradeParty.vue'
+import PayeeTradePartyAddress from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradePartyAddress.vue'
+export default{
+  components: {
+    PayeeEmail,
+    PayeeSpecifiedTaxRegistration,
+    PayeeTradeParty,
+    PayeeTradePartyAddress
+  },
+  props: ['PayeeProps']
+}
+</script>
