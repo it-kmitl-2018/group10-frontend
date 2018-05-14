6645caca59411a17c22cdf9eb3e79ad4126327fb
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 20:19:43 +0700

    Create SpecifiedTradeProduct component

---
+++ src/components/Form-Page/SpecificationOfCommodities/components/SpecifiedTradeProduct.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    รหัสสินค้า : <input v-model="specifiedTradeProductProp.id"/><br>
+    รหัสสินค้าสากล : <input v-model="specifiedTradeProductProp.globalID"/><br>
+    ชื่อสินค้า : <input v-model="specifiedTradeProductProp.name"/><br>
+    รายละเอียดสินค้า : <input v-model="specifiedTradeProductProp.description"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['specifiedTradeProductProp']
+}
+</script>
