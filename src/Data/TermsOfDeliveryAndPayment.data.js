1b641dfaafc838d61b5a33ac56a640139dcd0b74
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 16:34:28 +0700

    Create data model

---
+++ src/Data/TermsOfDeliveryAndPayment.data.js
@@ -0,0 +1,37 @@
+export default {
+  data: {
+    actualDeliverySupplyChainEvent: {
+      occurrenceDateTime: ''
+    },
+    shipToTradeParty: {
+      id: '',
+      globalID: '',
+      name: ''
+    },
+    definedTradeContact: {
+      personName: '',
+      departmentName: ''
+    },
+    telephoneUniversalCommunication: {
+      completeNumber: ''
+    }
+    postalTradeAddress: {
+      postcodeCode: '',
+      buildingName: '',
+      lineOne: '',
+      lineTwo: '',
+      lineThree: '',
+      lineFour: '',
+      lineFive: '',
+      streetName: '',
+      cityName: '',
+      citySubDivisionName: '',
+      countryID: '',
+      countrySubDivisionID: '',
+      buildingNumber: ''
+    }
+    applicableTradeDeliveryTerms: {
+      deliveryTypeCode: ''
+    }
+  }
+}
