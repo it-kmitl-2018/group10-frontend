b267e2d2f4131ea0fad76e671d2123bd68ffe1c3
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 20:17:04 +0700

    Create GrossPriceProductTradePrice component

---
+++ src/components/Form-Page/SpecificationOfCommodities/components/GrossPriceProductTradePrice.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    ราคาต่อหน่วย : <input v-model="grossPriceProductTradePriceProp.chargeAmount"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['grossPriceProductTradePriceProp']
+}
+</script>
