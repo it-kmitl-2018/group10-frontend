9c492a39c4136ba85d0856f1937a3658c2936503
aliveable <aliveable1754@gmail.com>
2018-05-14 19:35:22 +0700

    Change props variable to lower camel-case 2

--- src/components/Form-Page/PayerPayeeTradePartyDocument/electronicTaxInvoiceForm.vue
+++ src/components/Form-Page/PayerPayeeTradePartyDocument/electronicTaxInvoiceForm.vue
@@ -4,7 +4,7 @@
     <h1>ใบเสร็จอิเล็กทรอนิกส์</h1>
     <div>
       <div style="display:inline-block;margin:2%">
-        <PayeeTradePartyForm :PayeeProps="payeeTradePartyData"></PayeeTradePartyForm>
+        <PayeeTradePartyForm :payeeProps="payeeTradePartyData"></PayeeTradePartyForm>
       </div>
     </div>
     <br>
