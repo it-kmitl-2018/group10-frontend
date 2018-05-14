470e50118f920293a3fb5aae92f1cd7b9330e9a7
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 16:36:47 +0700

    Create ApplicableTradeDeliveryTerms components

---
+++ src/components/Form-Page/TermsOfDeliveryAndPayment/components/ApplicableTradeDeliveryTerms.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    รหัสเงื่อนไขการซื้อขาย: <input v-model="applicableTradeDeliveryTermsProp.deliveryTypeCode"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['applicableTradeDeliveryTermsProp']
+}
+</script>
