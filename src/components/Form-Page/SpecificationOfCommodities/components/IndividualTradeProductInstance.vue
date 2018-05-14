4c6b0868557fd27ddf0b2f7fd6fd6be65c7d6508
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 20:18:30 +0700

    Create IndividualTradeProductInstance component

---
+++ src/components/Form-Page/SpecificationOfCommodities/components/IndividualTradeProductInstance.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    ครั้งที่ผลิต : <input v-model="individualTradeProductInstanceProp.batchID"/><br>
+    วันหมดอายุ : <input v-model="individualTradeProductInstanceProp.expiryDateTime"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['individualTradeProductInstanceProp']
+}
+</script>
