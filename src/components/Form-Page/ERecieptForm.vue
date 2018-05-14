e7ac6ca3867e16b8c3d37c641127a31b93eb20ac
Unknown <kuro-neko@hotmail.co.th>
2018-05-14 13:20:52 +0700

    Add SellForm and PayeeForm to ERecieptForm.vue
    เพิ่ม SellerForm และ PayeeForm ลงใน ERecieptForm.vue

--- src/components/Form-Page/ERecieptForm.vue
+++ src/components/Form-Page/ERecieptForm.vue
@@ -2,6 +2,9 @@
   <div>
     <div>
       <div style="display:inline-block;margin:2%">
+        <seller-form :sellerForm="seller"></seller-form>
+      </div>
+      <div style="display:inline-block;margin:2%">
         <recipient-form :recipientProp="recipient"></recipient-form>
       </div>
       <div style="display:inline-block;margin:2%">
@@ -10,6 +13,9 @@
       <div style="display:inline-block;margin:2%">
         <document-recipient-form :documentRecipientFormProp="documentRecipient"></document-recipient-form>
       </div>
+      <div style="display:inline-block;margin:2%">
+        <payee-form :PayeeProps="payee"></payee-form>
+      </div>
     </div>
     <router-link :to="{ name: 'ERecieptConfirm'}">
       <button>ยืนยัน</button>
@@ -17,24 +23,32 @@
   </div>
 </template>
 <script>
+import SellerForm from '@/components/Form-Page/SellerForm/SellerForm.vue'
+import SellerData from '@/Data/Seller.data.js'
 import RecipientForm from '@/components/Form-Page/Recipient/RecipientForm.vue'
 import RecipientData from '@/Data/Recipient.data.js'
 import TaxInvoiceForm from '@/components/Form-Page/TaxInvoice/TaxInvoiceForm.vue'
 import TaxInvoiceData from '@/Data/TaxInvoice.data.js'
 import DocumentRecipientForm from '@/components/Form-Page/DocumentRecipient/DocumentRecipientForm.vue'
 import DocumentRecipientData from '@/Data/DocumentRecipient.data.js'
+import PayeeForm from '@/components/Form-Page/PayerPayeeTradePartyDocument/Payee-components/PayeeTradePartyForm.vue'
+import PayeeData from '@/Data/PayerPayeeTradeParty.data.js'
 export default {
   data () {
     return {
+      seller: SellerData.data,
       recipient: RecipientData.data,
       taxInvoice: TaxInvoiceData.data,
-      documentRecipient: DocumentRecipientData.data
+      documentRecipient: DocumentRecipientData.data,
+      payee: PayeeData.data
     }
   },
   components: {
+    SellerForm
     RecipientForm,
     TaxInvoiceForm,
-    DocumentRecipientForm
+    DocumentRecipientForm,
+    PayeeForm
   },
   props: ['documentRecipientProp']
 }
