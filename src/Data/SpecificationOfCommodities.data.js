f97ce2c84f63319d45a639bb311cc39bc95bc679
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 20:22:14 +0700

    Create SpecificationOfCommodities data model

---
+++ src/Data/SpecificationOfCommodities.data.js
@@ -0,0 +1,41 @@
+export default {
+  data: {
+    associatedDocumentLineDocument: {
+      lineID: ''
+    },
+    specifiedTradeProduct: {
+      id: '',
+      globalID: '',
+      name: '',
+      description: ''
+    },
+    individualTradeProductInstance: {
+      batchID: '',
+      expiryDateTime: ''
+    },
+    designatedProductClassification: {
+      className: ''
+    },
+    originTradeCountry: {
+      id: ''
+    },
+    includedNote: {
+      subject: '',
+      content: ''
+    },
+    grossPriceProductTradePrice: {
+      chargeAmount: ''
+    },
+    appliedTradeAllowanceCharge: {
+      actualAmount: '',
+      reasonCode: '',
+      reason: ''
+    },
+    applicableTradeTax: {
+      typeCode: '',
+      calculatedRate: '',
+      basisAmount: '',
+      calculatedAmount: ''
+    }
+  }
+}
