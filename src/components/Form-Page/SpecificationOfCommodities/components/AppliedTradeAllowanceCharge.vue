05d2ea09eb95e5b119ba711be8c93ba6ee370c73
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 20:15:09 +0700

    Create AppliedTradeAllowanceCharge component

---
+++ src/components/Form-Page/SpecificationOfCommodities/components/AppliedTradeAllowanceCharge.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    มูลค่าส่วนลด และค่าธรรมเนียม(ต่อหน่วย) : <input v-model="appliedTradeAllowanceChargeProp.actualAmount"/><br>
+    ประเภทค่าธรรมเนียม(ต่อหน่วย) : <input v-model="appliedTradeAllowanceChargeProp.reasonCode"/><br>
+    เหตุผลในการคิดค่าธรรมเนียมหรือส่วนลด : <input v-model="appliedTradeAllowanceChargeProp.reason"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['appliedTradeAllowanceChargeProp']
+}
+</script>
