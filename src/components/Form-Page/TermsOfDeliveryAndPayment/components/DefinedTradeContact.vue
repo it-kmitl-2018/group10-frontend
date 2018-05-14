c46db0dc35f92463f22b6455e1f1f5d6b104c21b
XenXorX <it58070024@office365.it.kmitl.ac.th>
2018-05-14 16:37:43 +0700

    Create DefinedTradeContact components

---
+++ src/components/Form-Page/TermsOfDeliveryAndPayment/components/DefinedTradeContact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    ชื่อผู้ติดต่อ : <input v-model="definedTradeContactProp.personName"/><br>
+    ชื่อแผนก : <input v-model="definedTradeContactProp.departmentName"/><br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['definedTradeContactProp']
+}
+</script>
